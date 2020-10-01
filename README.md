e5fd9bd2fbd549b45960fb704b1d526e2bc60493
Michael Cardillo <mike@mjcardillo.com>
2020-09-30 18:24:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# cl-sniper
+
+cl-sniper is a keras-based neural network model that can be trained to identify a specific item on Craigslist and provide notifications.
+
+## Disclaimer
+
+Craigslist is notoriously harsh on scrapers and bots. **Use reasonably and at your own risk!**
+
+### Installation
+
+Python dependencies can be install from the `requirements.txt` file
+
+```sh
+pip install -r ./requirements.txt
+```
+
+TensorFlow is also required, but not included in `requirements.txt`, as it is recommended to install this via your OS package manager.
+
+### Training
+
+You will need to provide a set of positive and negative images of the desired object. Positive images should be placed in `./images/yes` and negative images should be placed in `./images/no`.
+
+Once the images are in place, you can train your model by running
+
+`python ./trainer.py`
+
+### Scanning
+
+Once the model has been trained, you will need to set some configurations in `scanner.py`. 
+
+| Name            | Description                                                         |
+| --------------- | ------------------------------------------------------------------- |
+|   CL_RSS_FEED   | Provide the URL to the Craigslist RSS feed (note the 'query' field) |
+|    MODEL_PATH   | Full path to the saved epoch .h5 file during your model training    |
+| SCORE_THRESHOLD | Float from 1.0 to 0.0 of when you would like notifcations sent      |
+
+The scanner can then be run by issuing
+
+`python ./scanner.py`
---
+++ scanner.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import requests
+import feedparser
+from PIL import Image
+
+from io import BytesIO
+import os
+
+CL_RSS_FEED = "https://sfbay.craigslist.org/search/sss?format=rss&query=chair&sort=date"
+MODEL_PATH = "/path/to/savedmodel.h5"
+SCORE_THRESHOLD = 0.7
+
+# Headers copied from Firefox for Linux v81.0
+headers_xml = {
+    'Host': 'sfbay.craigslist.org',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Referer': 'https://sfbay.craigslist.org/search/sss?sort=date&query=chair',
+    'Upgrade-Insecure-Requests': '1',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache'
+}
+
+headers_img = {
+    'Host': 'images.craigslist.org',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'Accept': 'image/webp,*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Referer': 'https://sfbay.craigslist.org/search/sss?sort=date&query=chair',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache'
+}
+
+def fetch_and_test_image(url, model):
+    tmp_file_name = "tmp_img.jpg"
+    r = requests.get(url, headers=headers_img)
+
+    img = Image.open(BytesIO(r.content))
+    img.save(tmp_file_name)
+
+    image_size = (150, 150)
+
+    img = keras.preprocessing.image.load_img(
+        tmp_file_name, target_size=image_size
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+
+    os.remove("./tmp_img.jpg")
+    return predictions[0][0]
+
+r = requests.get(CL_RSS_FEED, headers=headers_xml)
+
+feed = feedparser.parse(r.content)
+model = keras.models.load_model(MODEL_PATH)
+scores = []
+for entry in feed.entries:
+    try:
+        score = fetch_and_test_image(entry['enc_enclosure']['resource'], model)
+        if score >= SCORE_THRESHOLD:
+            print(entry['link'], score)
+    except:
+        pass
