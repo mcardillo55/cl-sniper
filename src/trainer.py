7d70ffdc9647a16c267c8a4b72aeb5068f86f35b
Mike Cardillo <mike@mjcardillo.com>
2020-10-06 00:32:07 -0700

    Plot error and accuracy after training

--- src/trainer.py
+++ src/trainer.py
@@ -2,6 +2,25 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+def plot_history(history):
+    fig, axs = plt.subplots(2)
+
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
 
 def make_model(input_shape, num_classes):
     inputs = keras.Input(shape=input_shape)
@@ -73,6 +92,8 @@ model.compile(
     loss="binary_crossentropy",
     metrics=["accuracy"],
 )
-model.fit(
+history = model.fit(
     train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
 )
+
+plot_history(history)
